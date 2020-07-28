665f3f6fdf6ab189a447a681a9ff316a96501bfb
Nitin Kumar Jain <nitinjn09@gmail.com>
2020-07-28 17:04:25 +0530

    Initial commit

---
+++ sas-tool/src/main/java/com/tnps/sas/sastool/SasToolApplication.java
@@ -0,0 +1,13 @@
+package com.tnps.sas.sastool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SasToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SasToolApplication.class, args);
+	}
+
+}
---
+++ sas-tool/src/test/java/com/tnps/sas/sastool/SasToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnps.sas.sastool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SasToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
