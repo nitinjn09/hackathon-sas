7f4ecbf60f61599561b8e2be4efc9cd30052cb93
Nitin Kumar Jain <nitinjn09@gmail.com>
2020-08-04 11:08:43 +0530

    pm change: removed path hardcoding

--- project-manager/src/main/java/com/example/projectmanager/ComponentConfig.java
+++ project-manager/src/main/java/com/example/projectmanager/ComponentConfig.java
@@ -1,26 +1,31 @@
 package com.example.projectmanager;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ComponentConfig
 {
-    public boolean featureOneEnable = true;
-    public boolean featureTwoEnable = true;
+    public static String featureOneEnable = "featureOneEnable";
+    public static String featureTwoEnable = "featureTwoEnable";
+    public static Map<String, String> configMap = new HashMap<>();
+    private static ComponentConfig instance;
 
-    public ComponentConfig() {
+    public static ComponentConfig getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new ComponentConfig();
+        }
+        return instance;
     }
 
-    public boolean isFeatureOneEnable() {
-        return featureOneEnable;
+    private ComponentConfig()
+    {
+        configMap.put(featureOneEnable, Boolean.toString(true));
+        configMap.put(featureTwoEnable, Boolean.toString(true));
     }
 
-    public void setFeatureOneEnable(boolean featureOneEnable) {
-        this.featureOneEnable = featureOneEnable;
-    }
-
-    public boolean isFeatureTwoEnable() {
-        return featureTwoEnable;
-    }
-
-    public void setFeatureTwoEnable(boolean featureTwoEnable) {
-        this.featureTwoEnable = featureTwoEnable;
+    public static Map<String, String> getConfigMap() {
+        return configMap;
     }
 }
--- project-manager/src/main/java/com/example/projectmanager/ProjectController.java
+++ project-manager/src/main/java/com/example/projectmanager/ProjectController.java
@@ -1,15 +1,9 @@
 package com.example.projectmanager;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.YamlMapFactoryBean;
-import org.springframework.beans.factory.config.YamlProcessor;
-import org.springframework.boot.json.YamlJsonParser;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -18,6 +12,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 @Slf4j
@@ -25,9 +23,6 @@ public class ProjectController
 {
     @Value("${logging.file.name}")
     String logFile;
-
-    @Value("${conf.path}")
-    String confDir;
 
     @GetMapping("/test")
     public String collectLogs()
@@ -57,24 +52,23 @@ public class ProjectController
 
     @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/upload-conf", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String fileUpload(@RequestParam("File") MultipartFile file) throws IOException, ParseException {
-        //File convertFile = new File(confDir + "\\" + file.getOriginalFilename());
-        File convertFile = new File("C:\\Users\\prastogi\\Desktop\\" + file.getOriginalFilename());
-        convertFile.createNewFile();
-        FileReader reader = new FileReader(convertFile);
-        //JSONParser jsonParser = new JSONParser(reader);
-        //Object obj = jsonParser.parse();
-        //Yaml yaml = new Yaml();
-
-        ObjectMapper mapper = new ObjectMapper();
-        ComponentConfig componentConfig = mapper.readValue(reader, ComponentConfig.class);
-        System.out.println(componentConfig);
-
-        //ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        //FileOutputStream fout = new FileOutputStream(convertFile);
-        //fout.write(file.getBytes());
-        //fout.close();
-        return "File is upload successfully";
+    public String fileUpload(@RequestParam("File") MultipartFile file) throws IOException, ParseException
+    {
+        String content = new String(file.getBytes(), StandardCharsets.UTF_8);
+        JSONParser jsonParser = new JSONParser(content);
+        Object obj = jsonParser.parse();
+        Set<Map.Entry> entrySet = ((HashMap) obj).entrySet();
+        Map<String, String> configMap = ComponentConfig.getInstance().getConfigMap();
+        for(Map.Entry entry : entrySet)
+        {
+            String key = (String) entry.getKey();
+            String val = (String) entry.getValue();
+            if(configMap.containsKey(key))
+            {
+                configMap.put(key, val);
+            }
+        }
+        return "Config updated to:\n" + configMap.toString();
     }
 	
 	@CrossOrigin(origins = "http://localhost:4200")
